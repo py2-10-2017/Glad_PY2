248f471126993e3dfb916e7a3780277c92093b56
Duangdao <patavanich.d@gmail.com>
2017-10-21 16:10:22 -0700

    Add Book_Authors Assignment

--- dojo_ninjas/dojo_ninjas/settings.py
+++ dojo_ninjas/dojo_ninjas/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'apps.dojo_ninja',
+    'apps.book_authors',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
